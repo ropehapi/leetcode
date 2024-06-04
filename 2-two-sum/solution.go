9d3618d8624ca2dd97a743a29bf4e66b19aba32d
ropehapi <ropehapi@gmail.com>
2024-06-04 14:52:07 -0300

    Two sum improved to O(n)

--- 2-two-sum/solution.go
+++ 2-two-sum/solution.go
@@ -6,6 +6,10 @@ func main() {
 	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 	fmt.Println(twoSum([]int{3, 2, 4}, 6))
 	fmt.Println(twoSum([]int{3, 3}, 6))
+
+	fmt.Println(twoSumOpt([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSumOpt([]int{3, 2, 4}, 6))
+	fmt.Println(twoSumOpt([]int{3, 3}, 6))
 }
 
 func twoSum(nums []int, target int) []int {
@@ -18,4 +22,22 @@ func twoSum(nums []int, target int) []int {
 		}
 	}
 	return values
+}
+
+func twoSumOpt(nums []int, target int) []int {
+	table := make(map[int]int)
+	answer := make([]int, 2)
+
+	for key, value := range nums {
+		diff := target - value
+		if indice, ok := table[diff]; ok {
+			answer[0] = indice
+			answer[1] = key
+			break
+		}
+
+		table[value] = key
+	}
+
+	return answer
 }
